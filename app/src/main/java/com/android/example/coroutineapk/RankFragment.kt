65461f59ce34c56cdc60699131fb38e5cedb44cf
Maurizio Lopatriello <Carlolp69@gmail.com>
2022-11-26 10:23:01 +0100

    image game list 2

--- app/src/main/java/com/android/example/coroutineapk/RankFragment.kt
+++ app/src/main/java/com/android/example/coroutineapk/RankFragment.kt
@@ -18,6 +18,7 @@ import kotlinx.coroutines.launch
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
+import okhttp3.internal.connection.RealConnectionPool.Companion.get
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
